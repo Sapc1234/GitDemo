4d723d8a2141eadfb27ec25262ed7e9b9b0399c5
hp <sharanpadashetty637@gmail.com>
2021-09-10 16:45:32 +0530

    this is commit from asian timezone guys

--- src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -2,21 +2,20 @@ package qaclickacademy.Mavenjava;
 
 import org.testng.annotations.Test;
 
-public class RestAPITest 
-{
-	
+public class RestAPITest {
+
 	@Test
-	public void postJira()
-	{
+	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
 	}
-	
+
 	@Test
-	public void deleteTwitter()
-	{
+	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
 	}
-	
-	
 
 }
